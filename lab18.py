8154f8be346cd3cf966ac2d9ef0acbc6d2602e3e
jojogmattam <jojogmattam@gmail.com>
2023-10-30 20:57:05 +0000

    join and split

---
+++ lab18.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def main():
+
+    name_input = input("Please enter your name: ")
+    day_input = input("What day of the week it is: ")
+    print("Hello ", name_input, "! Happy ", day_input, "!", sep="")
+
+main()
