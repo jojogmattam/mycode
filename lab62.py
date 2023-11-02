1b6398325c92b68842b2c61cb79e463390c09591
jojogmattam <jojogmattam@gmail.com>
2023-11-02 17:19:46 +0000

    Updated code for lab62

--- lab62.py
+++ lab62.py
@@ -1,49 +1,82 @@
 #!/usr/bin/env python3
 
-farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+dataset1 = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
          {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
          {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
 
-print()
-print("Write a for loop that returns all the animals from the NE Farm!")
-print()
+dataset2 = [{"name": "Southwest Farm", "agriculture": ["chickens", "carrots", "celery"]},
+         {"name": "Northeast Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "East Farm", "agriculture": ["bananas", "apples", "oranges"]},
+         {"name": "West Farm", "agriculture": ["pigs", "chickens", "llamas"]}]
 
-for farm in farms:
-    if farm["name"] == "NE Farm":
-        for animal in farm["agriculture"]:
-            print(animal)
-        break
-
-print()
-print("Ask a user to choose a farm (NE Farm, W Farm, or SE Farm). Return the plants/animals that are raised on that farm.")
-print()
-
-input_farm = input("Choose a farm (NE Farm, W Farm, or SE Farm): ").lower()
-
-for farm in farms:
-    if farm["name"].lower() == input_farm:
-        for animal in farm["agriculture"]:
-            print(animal)
-        break
+def main():
+    input_dataset = input("Choose a dataset (1 or 2): ")
+    if input_dataset == "1":
+        farms = dataset1
+        print(f"You chose dataset {input_dataset}")
+        print()
+    elif input_dataset == "2":
+        farms = dataset2
+        print(f"You chose dataset {input_dataset}")
+        print()
     else:
-        continue
+        print("Invalid input")
+        exit()
+        
+    lab_action(farms)
 
-print()
-print("Ask a user to choose a farm (NE Farm, W Farm, or SE Farm)... but only return ANIMALS from that particular farm.")
-print()
 
-input_farm = input("Choose a farm (NE Farm, W Farm, or SE Farm): ").lower()
-plants = ["carrots", "celery"]
 
-for farm in farms:
-    if farm["name"].lower() == input_farm:
-        for animal in farm["agriculture"]:
-            if animal not in plants:
+plants = ["carrots", "celery", "bananas", "apples", "oranges"]
+
+def lab_action(farms):
+    print()
+    print("Write a for loop that returns all the animals from the NE Farm/NorthEast Farm")
+    print()
+
+    for farm in farms:
+        if farm["name"] == "Northeast Farm":
+            for animal in farm["agriculture"]:
                 print(animal)
-        break
-    else:
-        continue
+            break
+        elif farm["name"] == "NE Farm":
+            for animal in farm["agriculture"]:
+                print(animal)
+            break
 
-print()
-print("Bye")
-print()
+    print()
+    print("Ask a user to choose a farm. Return the plants/animals that are raised on that farm.")
+    print()
+
+    input_farm = input(f"Choose a farm {[farm['name'] for farm in farms]}: ").lower()
+
+    for farm in farms:
+        if farm["name"].lower() == input_farm:
+            for animal in farm["agriculture"]:
+                print(animal)
+            break
+        else:
+            continue
+
+    print()
+    print("Ask a user to choose a farm (NE Farm, W Farm, or SE Farm)... but only return ANIMALS from that particular farm.")
+    print()
+
+    input_farm = input(f"Choose a farm {[farm['name'] for farm in farms]}: ").lower()
+
+    for farm in farms:
+        if farm["name"].lower() == input_farm:
+            for animal in farm["agriculture"]:
+                if animal not in plants:
+                    print(animal)
+            break
+        else:
+            continue
+
+    print()
+    print("Bye")
+    print()
+    
+
+if __name__ == "__main__":
+    main()
